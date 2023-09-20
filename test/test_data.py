19ffc12f46ddafdde76a67d9e5c3463ef049aa23
Lazula <26179473+Lazula@users.noreply.github.com>
2023-09-20 15:53:02 -0500

    Add data tests

--- test/test_data.py
+++ test/test_data.py
@@ -19,7 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
 import unittest
+
+import mapis_requests
 
 from mapis_data import *
 
@@ -67,3 +70,242 @@ class TestPrintStatus(unittest.TestCase):
             status_string(False, self.api, self.target, status_code=400),
             Strings.FAIL_WITH_CODE.format(api=self.api, target=self.target, status_code=400)
         )
+
+
+class TestAddAPIData(unittest.TestCase):
+    target = "127.0.0.1"
+    target_hash = "00000000000000000000000000000000"
+
+    def test_add_ip_api_data(self):
+        target_api_data = dict()
+        ip_api_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(ip_api_data).encode())
+        add_ip_api_data(target_api_data, response, self.target)
+        self.assertEqual(target_api_data["ip_api"], ip_api_data)
+
+        target_api_data = dict()
+        ip_api_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(ip_api_data).encode())
+        response.status_code = 400
+        add_ip_api_data(target_api_data, response, self.target)
+        self.assertFalse("ip_api" in target_api_data)
+
+
+    def test_add_shodan_data(self):
+        target_api_data = dict()
+        shodan_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(shodan_data).encode())
+        add_shodan_data(target_api_data, response, self.target)
+        self.assertEqual(target_api_data["shodan"], shodan_data)
+
+        target_api_data = dict()
+        shodan_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(shodan_data).encode())
+        response.status_code = 400
+        add_shodan_data(target_api_data, response, self.target)
+        self.assertFalse("shodan" in target_api_data)
+
+
+    def test_add_virustotal_data(self):
+        target_api_data = dict()
+        virustotal_data = { "timeout":0, "undetected":0, "harmless":0, "suspicious":0, "malicious":0 }
+        response = virustotal_data
+        add_virustotal_data(target_api_data, response, self.target)
+        self.assertEqual(target_api_data["vt"], virustotal_data)
+
+        target_api_data = dict()
+
+        response = "NotFoundError"
+        add_virustotal_data(target_api_data, response, self.target)
+        self.assertFalse("vt" in target_api_data)
+
+        response = "APIError"
+        add_virustotal_data(target_api_data, response, self.target)
+        self.assertFalse("vt" in target_api_data)
+
+
+    def test_add_threatcrowd_data(self):
+        target_api_data = dict()
+        threatcrowd_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(threatcrowd_data).encode())
+        add_threatcrowd_data(target_api_data, response, self.target)
+        self.assertEqual(target_api_data["tc"], threatcrowd_data)
+
+        target_api_data = dict()
+        threatcrowd_data = {"key": "value"}
+        response = mapis_requests.dummy_response(json.dumps(threatcrowd_data).encode())
+        response.status_code = 400
+        add_threatcrowd_data(target_api_data, response, self.target)
+        self.assertFalse("tc" in target_api_data)
+
+
+    def test_add_alienvault_otx_data_address(self):
+        target_api_data = dict()
+        url_data = {"key1": "value1"}
+        malware_data = {"key2": "value2"}
+        url_response = mapis_requests.dummy_response(json.dumps(url_data).encode())
+        malware_response = mapis_requests.dummy_response(json.dumps(malware_data).encode())
+        responses = (url_response, malware_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target)
+        expected_otx_data = {
+            "url": url_data,
+            "malware": malware_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        url_data = {"key1": "value1"}
+        malware_data = {"key2": "value2"}
+        url_response = mapis_requests.dummy_response(json.dumps(url_data).encode())
+        url_response.status_code = 400
+        malware_response = mapis_requests.dummy_response(json.dumps(malware_data).encode())
+        responses = (url_response, malware_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target)
+        expected_otx_data = {
+            "malware": malware_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        url_data = {"key1": "value1"}
+        malware_data = {"key2": "value2"}
+        url_response = mapis_requests.dummy_response(json.dumps(url_data).encode())
+        malware_response = mapis_requests.dummy_response(json.dumps(malware_data).encode())
+        malware_response.status_code = 400
+        responses = (url_response, malware_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target)
+        expected_otx_data = {
+            "url": url_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        url_data = {"key1": "value1"}
+        malware_data = {"key2": "value2"}
+        url_response = mapis_requests.dummy_response(json.dumps(url_data).encode())
+        url_response.status_code = 400
+        malware_response = mapis_requests.dummy_response(json.dumps(malware_data).encode())
+        malware_response.status_code = 400
+        responses = (url_response, malware_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target)
+        self.assertFalse("otx" in target_api_data)
+
+
+    def test_add_alienvault_otx_api_data_hash(self):
+        target_api_data = dict()
+        general_data = {"key1": "value2"}
+        analysis_data = {"key2": "value2"}
+        general_response = mapis_requests.dummy_response(json.dumps(general_data).encode())
+        analysis_response = mapis_requests.dummy_response(json.dumps(analysis_data).encode())
+        responses = (general_response, analysis_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target_hash)
+        expected_otx_data = {
+            "general": general_data,
+            "analysis": analysis_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        general_data = {"key1": "value1"}
+        analysis_data = {"key2": "value2"}
+        general_response = mapis_requests.dummy_response(json.dumps(general_data).encode())
+        general_response.status_code = 400
+        analysis_response = mapis_requests.dummy_response(json.dumps(analysis_data).encode())
+        responses = (general_response, analysis_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target_hash)
+        expected_otx_data = {
+            "analysis": analysis_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        general_data = {"key1": "value1"}
+        analysis_data = {"key2": "value2"}
+        general_response = mapis_requests.dummy_response(json.dumps(general_data).encode())
+        analysis_response = mapis_requests.dummy_response(json.dumps(analysis_data).encode())
+        analysis_response.status_code = 400
+        responses = (general_response, analysis_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target_hash)
+        expected_otx_data = {
+            "general": general_data
+        }
+        self.assertEqual(target_api_data["otx"], expected_otx_data)
+
+        target_api_data = dict()
+        general_data = {"key1": "value1"}
+        analysis_data = {"key2": "value2"}
+        general_response = mapis_requests.dummy_response(json.dumps(general_data).encode())
+        general_response.status_code = 400
+        analysis_response = mapis_requests.dummy_response(json.dumps(analysis_data).encode())
+        analysis_response.status_code = 400
+        responses = (general_response, analysis_response)
+        add_alienvault_otx_data(target_api_data, responses, self.target_hash)
+        self.assertFalse("otx" in target_api_data)
+
+
+    def test_add_api_data_address(self):
+        target_api_data = dict()
+
+        ip_api_data = {"key": "value"}
+        ip_api_response = mapis_requests.dummy_response(json.dumps(ip_api_data).encode())
+        add_api_data("ip_api", target_api_data, ip_api_response, self.target)
+
+        shodan_data = {"key": "value"}
+        shodan_response = mapis_requests.dummy_response(json.dumps(shodan_data).encode())
+        add_shodan_data(target_api_data, shodan_response, self.target)
+
+        virustotal_data = { "timeout":0, "undetected":0, "harmless":0, "suspicious":0, "malicious":0 }
+        virustotal_response = virustotal_data
+        add_virustotal_data(target_api_data, virustotal_response, self.target)
+
+        threatcrowd_data = {"key": "value"}
+        threatcrowd_response = mapis_requests.dummy_response(json.dumps(threatcrowd_data).encode())
+        add_threatcrowd_data(target_api_data, threatcrowd_response, self.target)
+
+        url_data = {"key1": "value1"}
+        malware_data = {"key2": "value2"}
+        url_response = mapis_requests.dummy_response(json.dumps(url_data).encode())
+        malware_response = mapis_requests.dummy_response(json.dumps(malware_data).encode())
+        alienvault_otx_responses = (url_response, malware_response)
+        add_alienvault_otx_data(target_api_data, alienvault_otx_responses, self.target)
+
+        expected_api_data = {
+            "ip_api": ip_api_data,
+            "shodan": shodan_data,
+            "vt": virustotal_data,
+            "tc": threatcrowd_data,
+            "otx": {
+                "url": url_data,
+                "malware": malware_data
+            }
+        }
+        self.assertEqual(target_api_data, expected_api_data)
+
+
+    def test_add_api_data_hash(self):
+        target_api_data = dict()
+
+        virustotal_data = { "timeout":0, "undetected":0, "harmless":0, "suspicious":0, "malicious":0 }
+        virustotal_response = virustotal_data
+        add_virustotal_data(target_api_data, virustotal_response, self.target_hash)
+
+        threatcrowd_data = {"key": "value"}
+        threatcrowd_response = mapis_requests.dummy_response(json.dumps(threatcrowd_data).encode())
+        add_threatcrowd_data(target_api_data, threatcrowd_response, self.target_hash)
+
+        general_data = {"key1": "value1"}
+        analysis_data = {"key2": "value2"}
+        general_response = mapis_requests.dummy_response(json.dumps(general_data).encode())
+        analysis_response = mapis_requests.dummy_response(json.dumps(analysis_data).encode())
+        alienvault_otx_responses = (general_response, analysis_response)
+        add_alienvault_otx_data(target_api_data, alienvault_otx_responses, self.target_hash)
+
+        expected_api_data = {
+            "vt": virustotal_data,
+            "tc": threatcrowd_data,
+            "otx": {
+                "general": general_data,
+                "analysis": analysis_data
+            }
+        }
+        self.assertEqual(target_api_data, expected_api_data)
